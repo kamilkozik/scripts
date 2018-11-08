8ff590154b2154106bc05c2e7a5ef7d5ac8ed031
kamil <kamil.kozik@outlook.com>
2018-11-08 06:13:25 +0100

    Using HOME variable

--- radio.sh
+++ radio.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-echo "[`date`][$0] STARTING..." >> /home/kamil/radio.log
+echo "[`date`][$0] STARTING..." >> $HOME/radio.log
 amixer -D pulse sset Master 60%
 mplayer -volume 55 -nocache -afm ffmpeg http://lodz.radio.pionier.net.pl:8000/pl/tuba10-1.mp3?cache=1540235067_redir__e1_
-echo "[`date`][$0] FINISHED" >> /home/kamil/radio.log
+echo "[`date`][$0] FINISHED" >> $HOME/radio.log
